a535c7e7c8dbade75df002c26b41e995a089cc79
Andrea Konomi <andreakonomi23@gmail.com>
2022-04-07 23:56:50 +0200

    improved loading of configurations

--- ConsoleApp/src/main/java/com/demo/searchengine/console/Helper.java
+++ ConsoleApp/src/main/java/com/demo/searchengine/console/Helper.java
@@ -2,6 +2,8 @@ package com.demo.searchengine.console;
 
 import javax.naming.ConfigurationException;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class Helper {
@@ -9,23 +11,31 @@ public class Helper {
     /*/
     reads the configuration specified by the requested key
      */
-    public static String readFromConfigs(String key) throws ConfigurationException {
+    public static String readFromConfigs(String key) throws ConfigurationException, FileNotFoundException {
+        String result = "";
+        InputStream inputStream;
+
         try{
             Properties props = new Properties();
-            String fileName = "C:\\Users\\user\\Desktop\\Coder\\Java\\JSearchEngine\\ConsoleApp\\src\\main\\resources\\application.properties";
-
-            try (FileInputStream fis = new FileInputStream(fileName)) {
-                props.load(fis);
-            } catch (Exception ex) {
-                throw new ConfigurationException("There is a problem with the configurations file.");
+            String propFileName = "application.properties";
+            inputStream = Helper.class.getClassLoader().getResourceAsStream(propFileName);
+            if (inputStream != null){
+                props.load(inputStream);
+            }
+            else{
+                throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
             }
 
-            var valueFound =  props.getProperty(key);
-            if (valueFound == null){
+            result = props.getProperty(key);
+
+            if (result == null){
                 throw new ConfigurationException(String.format("The specified key: %s was not found in the configurations file", key));
             }
 
-            return valueFound;
+            return result;
+        }
+        catch(FileNotFoundException e){
+            throw e;
         }
         catch(Exception ex){
             throw new ConfigurationException("There is a problem with the configurations file.");
